914a1e8174877164dcca643eef05010c2fc6b0a2
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2024-05-05 16:48:45 +0330

    layout project

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import 'font-awesome/css/font-awesome.min.css' 
-function Header() {
+function Header({children}) {
   return (
     <>
+    <header>
     <div className='flex border justify-between w-2/4 m-auto mt-20 p-4 rounded-lg items-center shadow-md text-sky-100 border-sky-500 '>
         <h1 className='font-bold text-2xl font-sans'>Library-Project</h1>
         <p className='font-semibold font-sans p-1 '>The second project by ReactJs</p>
@@ -15,6 +16,14 @@ function Header() {
       <button><i className='fa fa-search text-slate-400 pl-11'></i></button>
     </form>
     </div>
+    </header>
+
+    {children}
+
+
+    <footer className='border flex m-auto w-3/5 p-3 border-2 rounded-full text-sky-200 font-sans font-semibold items-center justify-center shadow-xl mt-80'>
+      <h1>Developed by Hossein Kimiyavi</h1>
+    </footer>
     </>
   )
 }
